8636a35e6035e000dd24e505143ae3730f6660c3
Michael Dipperstein <mdipper@alumni.engr.ucsb.edu>
2010-03-08 19:42:25 -0800

    Updated for LGPL v3 release.

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 ############################################################################
 # Makefile for bitarray library and sample
 #
-#   $Id: Makefile,v 1.1.1.1 2004/02/09 04:15:45 michael Exp $
+#   $Id: Makefile,v 1.2 2007/07/16 01:55:50 michael Exp $
 #   $Log: Makefile,v $
+#   Revision 1.2  2007/07/16 01:55:50  michael
+#   Use -pedantic when compiling.
+#
 #   Revision 1.1.1.1  2004/02/09 04:15:45  michael
 #   Initial release
 #
@@ -11,7 +14,7 @@
 
 CC = gcc
 LD = gcc
-CFLAGS = -O2 -Wall -ansi -c
+CFLAGS = -O2 -Wall -pedantic -ansi -c
 LDFLAGS = -O2 -o
 
 # Treat NT and non-NT windows the same
@@ -22,22 +25,22 @@ endif
 ifeq ($(OS),Windows)
 	EXE = .exe
 	DEL = del
-else	#assume Linux/Unix
+else    #assume Linux/Unix
 	EXE =
 	DEL = rm
 endif
 
-all:		sample$(EXE)
+all:	sample$(EXE)
 
 sample$(EXE):	sample.o bitarray.o
-		$(LD) $^ $(LDFLAGS) $@
+	$(LD) $^ $(LDFLAGS) $@
 
 sample.o:	sample.c bitarray.h
-		$(CC) $(CFLAGS) $<
+	$(CC) $(CFLAGS) $<
 
 bitarray.o:	bitarray.c bitarray.h
-		$(CC) $(CFLAGS) $<
+	$(CC) $(CFLAGS) $<
 
 clean:
-		$(DEL) *.o
-		$(DEL) sample$(EXE)
+	$(DEL) *.o
+	$(DEL) sample$(EXE)
--- sample.c
+++ sample.c
@@ -9,8 +9,11 @@
 ****************************************************************************
 *   HISTORY
 *
-*   $Id: sample.c,v 1.2 2006/02/08 14:03:21 michael Exp $
+*   $Id: sample.c,v 1.3 2007/08/26 21:20:20 michael Exp $
 *   $Log: sample.c,v $
+*   Revision 1.3  2007/08/26 21:20:20  michael
+*   Changes required for LGPL v3.
+*
 *   Revision 1.2  2006/02/08 14:03:21  michael
 *   Update for latest changes to bitarray.c
 *
@@ -21,21 +24,22 @@
 ****************************************************************************
 *
 * Sample: A bit array library sample usage program
-* Copyright (C) 2004 by Michael Dipperstein (mdipper@cs.ucsb.edu)
+* Copyright (C) 2004, 2006-2007 by Michael Dipperstein (mdipper@cs.ucsb.edu)
 *
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation; either
-* version 2.1 of the License, or (at your option) any later version.
+* This file is part of the bit array library.
 *
-* This library is distributed in the hope that it will be useful,
+* The bit array library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public License as
+* published by the Free Software Foundation; either version 3 of the
+* License, or (at your option) any later version.
+*
+* The bit array library is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Lesser General Public License for more details.
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+* General Public License for more details.
 *
-* You should have received a copy of the GNU Lesser General Public
-* License along with this library; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *
 ***************************************************************************/
 #include <stdio.h>
